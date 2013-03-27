95050f2849383411de91192eff7b242ee67de260
Garret Alfert <alfert@wevelop.de>
2013-03-27 18:31:47 +0100

    Test for createRecord

--- packages/ember-data-remotestorage/tests/adapter_test.js
+++ packages/ember-data-remotestorage/tests/adapter_test.js
@@ -1,3 +1,5 @@
+var get = Ember.get, set = Ember.set;
+
 var adapter, store;
 var Todo, todo, todos;
 
@@ -27,6 +29,21 @@ module("DS.RSAdapter", {
   }
 });
 
+var expectState = function(state, value, t) {
+  t = t || todo;
+
+  if (value === undefined) { value = true; }
+
+  var flag = "is" + state.charAt(0).toUpperCase() + state.substr(1);
+  equal(get(t, flag), value, "the todo is " + (value === false ? "not " : "") + state);
+};
+
+var expectStates = function(state, value) {
+  todos.forEach(function(todo) {
+    expectState(state, value, todo);
+  });
+};
+
 test("existence", function() {
   ok(DS.RSAdapter, "RSAdapter added to DS namespace");
   ok(remoteStorage, "remoteStorage exists");
@@ -35,4 +52,17 @@ test("existence", function() {
 test("find", function() {
   var list = [];
   equal(list.length, 0, "list is empty");
+});
+
+test("create a todo", function() {
+  Ember.run(function() {
+    todo = store.createRecord(Todo, { title: "Homework" });
+  });
+
+  expectState("new");
+
+  Ember.run(function() {
+    store.commit();
+    expectState("saving");
+  });
 });
